4414a4c9e08f244e354be41f9e789693655b19ff
jackv471 <96673252+jackv471@users.noreply.github.com>
2024-10-21 09:50:14 +1100

    Added footstep effects depending on floor type

---
+++ scenes/characters/FootstepSoundManager.gd
@@ -0,0 +1,21 @@
+extends RayCast3D
+
+@onready var footsteps_player: FootstepsPlayer = $".."
+
+
+# 0 = dirt
+#1 = wood
+#2 = stone
+@export var footstepSounds: Array[AudioStream]
+
+
+func _physics_process(delta: float) -> void:
+	if is_colliding():
+		var groundType := get_collider().get_meta("GroundType",-1) as int
+		print(footsteps_player.stream)
+		if groundType != -1:
+			if footsteps_player.stream != footstepSounds[groundType]:
+				footsteps_player.stream = footstepSounds[groundType]
+		else:
+			push_error("no ground type assigned in metadata")
+
