05c7ca63879ff5ad8f36c2d133d3702ac3970e6e
jackv471 <96673252+jackv471@users.noreply.github.com>
2024-10-23 10:45:23 +1100

    Added bell tower and game ending when bell is rung while demon is alive

---
+++ scenes/objects/Bell.gd
@@ -0,0 +1,36 @@
+extends Interactable
+
+@onready var bell_sound: AudioStreamPlayer = $BellSound
+var gameOver := false
+const MAIN_MENU = preload("res://scenes/ui/main_menu.tscn")
+
+func interact():
+	interactable = false
+	super.interact()
+	var moveTween := create_tween().set_trans(Tween.TRANS_SINE)
+	moveTween.tween_property(self,"rotation:x",-PI/3,1)
+	moveTween.tween_property(self,"rotation:x",PI/4,2)
+	moveTween.tween_property(self,"rotation:x",-PI/5,2)
+	moveTween.tween_property(self,"rotation:x",PI/6,2)
+	moveTween.tween_property(self,"rotation:x",-PI/7,2)
+	moveTween.tween_property(self,"rotation:x",0,2)
+	await get_tree().create_timer(1).timeout
+	bell_sound.play()
+
+	if Demon.spawnedInstance:
+		PlayerHud.Instance.display_overlay_text("The Demon Mincinos Has Been Slain")
+		gameOver = true
+
+		var vfxTween := create_tween().set_parallel(true).set_parallel(true)
+		vfxTween.tween_property(Camera.Instance.environment,"volumetric_fog_density", 1.0,10.0)
+		vfxTween.tween_property(Camera.Instance.environment,"volumetric_fog_albedo", Color.DIM_GRAY, 10)
+		var low_pass:= AudioServer.get_bus_effect(0,3) as AudioEffectLowPassFilter
+		low_pass.cutoff_hz = 2000
+		AudioServer.set_bus_effect_enabled(0,5,true)
+		vfxTween.tween_property(low_pass,"cutoff_hz",0,10.0)
+		await vfxTween.finished
+		get_tree().quit()
+		return
+	await get_tree().create_timer(8).timeout
+	interactable = true
+
--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@export var main_scene: PackedScene
+const GAME_LEVEL = preload("res://scenes/levels/game_level.tscn")
 @onready var play_button: Button = %play_button
 @onready var options_button: Button = %options_button
 @onready var credits_button: Button = %credits_button
@@ -10,7 +10,7 @@ func _ready() -> void:
 	play_button.pressed.connect(new_game)
 	quit_button.pressed.connect(quit)
 func new_game():
-	get_tree().change_scene_to_packed(main_scene)
+	get_tree().change_scene_to_packed(GAME_LEVEL)
 
 func quit():
 	get_tree().quit()
