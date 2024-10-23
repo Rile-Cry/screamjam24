bfbdd95bf470b875881f72b3dfaef0ac1ef863b9
jackv471 <96673252+jackv471@users.noreply.github.com>
2024-10-23 13:52:28 +1100

    Did a little switcheroo so the ui shouldnt clash anymore

--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -1,10 +1,12 @@
 extends Control
 
 const GAME_LEVEL = preload("res://scenes/levels/game_level.tscn")
-@onready var play_button: Button = %play_button
-@onready var options_button: Button = %options_button
-@onready var credits_button: Button = %credits_button
-@onready var quit_button: Button = %quit_button
+@onready var play_button: Button = $BookMargin/book_cover/ButtonMargin/ButtonContainer/play_button
+@onready var options_button: Button = $BookMargin/book_cover/ButtonMargin/ButtonContainer/options_button
+@onready var credits_button: Button = $BookMargin/book_cover/ButtonMargin/ButtonContainer/credits_button
+@onready var quit_button: Button = $BookMargin/book_cover/ButtonMargin/ButtonContainer/quit_button
+
+
 
 func _ready() -> void:
 	play_button.pressed.connect(new_game)
